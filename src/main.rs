efe0ec020c5c87b9825e9444b2f5cd8a7b5079c1
JamiroFerrara <ferrarajamiro@hotmail.com>
2022-08-27 14:20:56 +0200

    Changed file structure

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use rdrive::directory::Directory;
+use clap::Parser;
 
 mod directory;
 
+///Argument parser for the start of the program
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    ///Path to search
+    n: String
+}
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let args = std::env::args().collect::<Vec<String>>();
-    let dir = directory::Directory::new(&args[1])?;
+    let args = Args::parse();
+
+    let dir = directory::Directory::new(&args.n)?;
     let files = dir.all_dirs();
     println!("{:?}", files);
 
