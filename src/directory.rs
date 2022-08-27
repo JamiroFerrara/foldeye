236852fa184f480eb84bc77ba955d2b01474810e
JamiroFerrara <ferrarajamiro@hotmail.com>
2022-08-27 11:26:02 +0200

    Implemented directory walker

--- src/directory.rs
+++ src/directory.rs
@@ -1,13 +1,17 @@
+use std::fs;
+use rayon::prelude::*;
+
 #[derive(Debug)]
-enum Entry {
+pub enum Entry {
     File(File),
     Directory(Directory),
 }
 
+
 #[derive(Debug)]
-struct File {
-    name: String,
-    size: i32,
+pub struct File {
+    pub name: String,
+    pub size: i32,
 }
 
 impl File {
@@ -19,39 +23,66 @@ impl File {
     }
 }
 
+
 #[derive(Debug)]
-struct Directory {
-    name: String,
-    entries: Vec<Entry>,
+pub struct Directory {
+    pub name: String,
+    pub entries: Vec<Entry>,
 }
 
 impl Directory {
-    fn new(name: &str) -> Directory {
-        Directory {
-            name: name.to_string(),
-            entries: Vec::new(),
+    pub fn new(path: &str) -> Result<Directory, std::io::Error> {
+        let entries = get_entries(path)?;
+
+        Ok(Directory {
+            name: path.to_string(),
+            entries: entries,
+        })
+    }
+
+    pub fn count(&self) -> i32 {
+        self.entries
+            .par_iter()
+            .map(|entry| match entry {
+                Entry::File(_) => 1,
+                Entry::Directory(dir) => dir.count(),
+            })
+            .sum()
+    }
+
+    pub fn dirs(&self) -> Vec<&String> {
+        self.entries
+            .par_iter()
+            .filter_map(|entry| match entry {
+                Entry::File(_) => None,
+                Entry::Directory(dir) => Some(&dir.name),
+            })
+            .collect()
+    }
+
+    pub fn files(&self) -> Vec<&String> {
+        self.entries
+            .par_iter()
+            .filter_map(|entry| match entry {
+                Entry::File(file) => Some(&file.name),
+                Entry::Directory(_) => None,
+            })
+            .collect()
+    }
+}
+
+fn get_entries(path: &str) -> Result<Vec<Entry>, std::io::Error> {
+    let read_dir = fs::read_dir(path)?;
+    let mut entries = Vec::new();
+    for dir in read_dir {
+        let entry = dir?;
+        let metadata = entry.metadata()?;
+        if metadata.is_dir() {
+            entries.push(Entry::Directory(Directory::new(&entry.path().to_str().unwrap())?));
+        } else {
+            entries.push(Entry::File(File::new(&entry.path().to_str().unwrap(), metadata.len() as i32)));
         }
     }
 
-    fn walk_directories(&mut self) -> Vec<Entry>{
-        let mut entries = Vec<Entry>::new();
-        for entry in &self.entries {
-            match entry {
-                Entry::Directory(dir) => {
-                    dir.walk_directories();
-                }
-                Entry::File(file) => {
-                    entries.push(file);
-                }
-            }
-        }
-    }
-
-    fn add_file(&mut self, file: File) {
-        self.entries.push(Entry(file));
-    }
-
-    fn add_directory(&mut self, directory: Directory) {
-        self.entries.push(Entry(directory));
-    }
+    Ok(entries)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use rdrive::directory::Directory;
+
 mod directory;
 
 #[tokio::main]
-fn main() {
+async fn main() -> Result<(), std::io::Error> {
+    let args = std::env::args().collect::<Vec<String>>();
+    let dir = directory::Directory::new(&args[1])?;
+
+    Ok(())
 }
